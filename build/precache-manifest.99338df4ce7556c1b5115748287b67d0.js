d9d80f3fcac00b7a085d6bc936f508b7f43ad0bb
Prince Rajput <drprincerajput@gmail.com>
2020-08-18 21:14:44 +0530

    gitignore change

---
+++ build/precache-manifest.99338df4ce7556c1b5115748287b67d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19680f3a29818c9c27d1a35f2534331b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a699aefb3d7439d2e63c",
+    "url": "/static/css/main.ee65317a.chunk.css"
+  },
+  {
+    "revision": "6300a85b472210a384ba",
+    "url": "/static/js/2.5ef86ee5.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.5ef86ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a699aefb3d7439d2e63c",
+    "url": "/static/js/main.698f3c7b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
